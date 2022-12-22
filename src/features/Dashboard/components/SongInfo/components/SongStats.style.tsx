8bd705e4b3921843e2728ffd88e0024655c37400
Kai Corstjens <kai_corstjens@hotmail.com>
2022-12-22 20:59:52 +0100

    Small styling updates, removed template code from index

--- src/features/Dashboard/components/SongInfo/components/SongStats.style.tsx
+++ src/features/Dashboard/components/SongInfo/components/SongStats.style.tsx
@@ -10,6 +10,8 @@ export const SongStatsContainer = styled.div`
   background-color: rgba(0, 0, 0, 0.5);
   padding-right: 25px;
   padding-left: 25px;
+  padding-top: 10px;
+  padding-bottom: 20px;
   border-radius: 0 25px 25px 0;
   color: white;
   float: left;
