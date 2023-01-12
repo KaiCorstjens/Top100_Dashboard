33983ef28c97c46a2d89291028b244bbc0eec912
Kai Corstjens <kai_corstjens@hotmail.com>
2023-01-12 22:58:44 +0100

    Updates

--- src/app/constants.ts
+++
@@ -1,2 +0,0 @@
-// spotify
-export const SPOTIFY_CLIENT_ID = "ba35578ed09c4c3f92a833d03ecbe575";
--- src/features/Dashboard/Dashboard.style.tsx
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-
-export const FullScreenDashboard = styled.div`
-  background: ${(props) => props.theme.background};
-  width: 100%;
-  height: 100%;
-  position: absolute;
-  background-size: cover;
-`;
